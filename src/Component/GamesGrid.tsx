d41a597309bba2e9bbae0ab7a7ed92245d1c64cd
Jayani Anuththara <jayanianuththara10@gmail.com>
2025-05-14 21:11:26 +0530

    Display Genres

--- src/Component/GamesGrid.tsx
+++ src/Component/GamesGrid.tsx
@@ -15,7 +15,7 @@ const GamesGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
         padding={10}
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
